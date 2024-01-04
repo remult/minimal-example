b1a31e95f9a15a4a8a4ac498474aec2e30ebbe32
Yoni Rapoport <yoni@fireflymigration.com>
2024-01-04 15:22:09 +0200

    change db to in-memory sqlite

--- server/api.ts
+++ server/api.ts
@@ -1,10 +1,18 @@
 import { remultExpress } from "remult/remult-express";
+import { createKnexDataProvider } from "remult/remult-knex";
 import { Product } from "shared/products.js";
 import { Customer } from "shared/customers.js";
 import { Order } from "shared/orders.js";
 import { OrderDetail } from "shared/order-details.js";
 
 export const api = remultExpress({
+  dataProvider: createKnexDataProvider({
+    client: "sqlite3",
+    connection: ":memory:",
+    debug: true,
+    log: { debug: ({ sql, bindings }) => console.log({ sql, bindings }) },
+    useNullAsDefault: false,
+  }),
   entities: [Product, Customer, Order, OrderDetail],
   getUser: (req) => req.session!["user"],
 });
